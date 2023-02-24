946027e6ab6a873f7baa4a92eea1d35a1bd955b9
MauR27 <antonio27mauricio27@gmail.com>
2023-02-24 16:45:17 +0100

    a;adi los audios y le di funcionalidad a los btn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const onClickEvent = (e) => {
+    const targetValue = e.target.value;
+    const Q = document.getElementById(targetValue);
+    Q.currentTime = "0";
+    Q.play();
+  };
+
+  document.addEventListener("keyup", function (e) {
+    const ka = e.key;
+    if (
+      e.key === "q" ||
+      e.key === "w" ||
+      e.key === "e" ||
+      e.key === "a" ||
+      e.key === "s" ||
+      e.key === "d" ||
+      e.key === "z" ||
+      e.key === "x" ||
+      e.key === "c"
+    ) {
+      const q = document.getElementById(ka);
+      q.currentTime = "0";
+      q.play();
+    }
+  });
+
   return (
     <div className="App">
       <div className="title">
@@ -9,43 +36,56 @@ function App() {
       <div id="drum-machine">
         <div id="display">
           <div className="drum-pad">
-            <button>Q</button>
-            <audio src=""></audio>
+            <button id="bass" onClick={onClickEvent} value="q">
+              Q<audio src="sounds/bass.mp4" className="clip" id="q"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>W</button>
-            <audio src=""></audio>
+            <button id="crash" onClick={onClickEvent} value="w">
+              W<audio src="sounds/crash.mp4" className="clip" id="w"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>E</button>
-            <audio src=""></audio>
+            <button id="floor-tom" onClick={onClickEvent} value="e">
+              E
+              <audio src="sounds/floor-tom.mp4" className="clip" id="e"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>A</button>
-            <audio src=""></audio>
+            <button id="hh-close" onClick={onClickEvent} value="a">
+              A<audio src="sounds/hh-close.mp4" className="clip" id="a"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>S</button>
-            <audio src=""></audio>
+            <button id="hh-open" onClick={onClickEvent} value="s">
+              S<audio src="sounds/hh-open.mp4" className="clip" id="s"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>D</button>
-            <audio src=""></audio>
+            <button id="med-tom" onClick={onClickEvent} value="d">
+              D<audio src="sounds/med-tom.mp4" className="clip" id="d"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>Z</button>
-            <audio src=""></audio>
+            <button id="ride" onClick={onClickEvent} value="z">
+              Z<audio src="sounds/ride.mp4" className="clip" id="z"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>X</button>
-            <audio src=""></audio>
+            <button id="small-tom" onClick={onClickEvent} value="x">
+              X
+              <audio src="sounds/small-tom.mp4" className="clip" id="x"></audio>
+            </button>
           </div>
           <div className="drum-pad">
-            <button>C</button>
-            <audio src=""></audio>
+            <button id="snare" onClick={onClickEvent} value="c">
+              C<audio src="sounds/snare.mp4" className="clip" id="c"></audio>
+            </button>
           </div>
         </div>
-        <div id="controls-box">hola</div>
+        <div id="controls-box">
+          <h2>Snare</h2>
+        </div>
       </div>
     </div>
   );
