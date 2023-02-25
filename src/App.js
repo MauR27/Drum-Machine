89feda1d4c375dd3f60c58046cb76272601f807c
MauR27 <antonio27mauricio27@gmail.com>
2023-02-25 03:58:54 +0100

    le hice toggle mode a los sonidos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   const [onOff, setOnOff] = useState(true);
   const [textOnOff, setTextOnOff] = useState("OFF");
 
-  const [rockEffects, setRockEffects] = useState("");
+  const [onOffToggle, setOnOffToggle] = useState(true);
+  const [urlToggle, setUrlToggle] = useState("rock");
 
   let soundEffects = {
     q: "Bass",
@@ -31,6 +32,20 @@ function App() {
     }
   };
 
+  const eventToggle = () => {
+    setOnOffToggle((prev) => !prev);
+    if (onOffToggle) {
+      setDisplayText("POP MODE");
+      setUrlToggle("pop");
+    } else {
+      setDisplayText("ROCK MODE");
+      setUrlToggle("rock");
+    }
+
+    console.log(urlToggle);
+    console.log(onOffToggle);
+    console.log();
+  };
   // AGREGAR SONIDO CON LOS BOTONES
 
   const onClickEvent = (e) => {
@@ -69,49 +84,92 @@ function App() {
         <div id="display">
           <div className="drum-pad">
             <button id="bass" onClick={onClickEvent} value="q">
-              Q<audio src="sounds/bass.mp4" className="clip" id="q"></audio>
+              Q
+              <audio
+                src={"sounds/" + urlToggle + "/bass.mp4"}
+                className="clip"
+                id="q"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="crash" onClick={onClickEvent} value="w">
-              W<audio src="sounds/crash.mp4" className="clip" id="w"></audio>
+              W
+              <audio
+                src={"sounds/" + urlToggle + "/crash.mp4"}
+                className="clip"
+                id="w"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="floor-tom" onClick={onClickEvent} value="e">
               E
-              <audio src="sounds/floor-tom.mp4" className="clip" id="e"></audio>
+              <audio
+                src={"sounds/" + urlToggle + "/floor-tom.mp4"}
+                className="clip"
+                id="e"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="hh-close" onClick={onClickEvent} value="a">
-              A<audio src="sounds/hh-close.mp4" className="clip" id="a"></audio>
+              A
+              <audio
+                src={"sounds/" + urlToggle + "/hh-close.mp4"}
+                className="clip"
+                id="a"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="hh-open" onClick={onClickEvent} value="s">
-              S<audio src="sounds/hh-open.mp4" className="clip" id="s"></audio>
+              S
+              <audio
+                src={"sounds/" + urlToggle + "/hh-open.mp4"}
+                className="clip"
+                id="s"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="med-tom" onClick={onClickEvent} value="d">
-              D<audio src="sounds/med-tom.mp4" className="clip" id="d"></audio>
+              D
+              <audio
+                src={"sounds/" + urlToggle + "/med-tom.mp4"}
+                className="clip"
+                id="d"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="ride" onClick={onClickEvent} value="z">
-              Z<audio src="sounds/ride.mp4" className="clip" id="z"></audio>
+              Z
+              <audio
+                src={"sounds/" + urlToggle + "/ride.mp4"}
+                className="clip"
+                id="z"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="small-tom" onClick={onClickEvent} value="x">
               X
-              <audio src="sounds/small-tom.mp4" className="clip" id="x"></audio>
+              <audio
+                src={"sounds/" + urlToggle + "/small-tom.mp4"}
+                className="clip"
+                id="x"
+              ></audio>
             </button>
           </div>
           <div className="drum-pad">
             <button id="snare" onClick={onClickEvent} value="c">
-              C<audio src="sounds/snare.mp4" className="clip" id="c"></audio>
+              C
+              <audio
+                src={"sounds/" + urlToggle + "/snare.mp4"}
+                className="clip"
+                id="c"
+              ></audio>
             </button>
           </div>
         </div>
@@ -120,13 +178,13 @@ function App() {
             <h2>{displayText}</h2>
           </div>
           <div className="volumen">
-            <input type="range" />
+            <input type="range" min="0" max="100" id="vol" />
             <h3>Volumen</h3>
           </div>
           <label className="switch">
-            <input type="checkbox" id="toggle" />
+            <input type="checkbox" id="toggle" onClick={eventToggle} />
             <span className="slider round"></span>
-            <p>toggle</p>
+            <p>Mode</p>
           </label>
           <label className="switch">
             <input type="checkbox" onClick={eventClick} />
